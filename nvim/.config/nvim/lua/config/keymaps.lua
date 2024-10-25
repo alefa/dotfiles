fd681d03f07dbe9d57bfae994c3fc639eddecb65
Gita Benadi <gita.benadi@posteo.de>
2024-10-25 09:47:24 +0200

    Add more custom keybindings to Neovim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 -- Close (kill) current buffer
 vim.keymap.set("n", "<leader>k", "<cmd>bdelete<CR>")
 
+-- List open buffers and select one
+vim.keymap.set("n", "<leader>b", ":ls<CR>:b<Space>")
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -27,3 +30,34 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 
 -- Use underscore instead of comma to repeat character search in the opposite direction:
 vim.keymap.set("n", "_", ",")
+
+-- Move by display lines with j/k, but not if these are preceded by a count.
+-- If they are preceded by a count, add these movements to the jumplist.
+function LineMotion(dir)
+    local count = vim.v.count1 > 1 and ("m'" .. vim.v.count1) or "g"
+    vim.cmd("normal! " .. count .. dir)
+end
+
+vim.keymap.set('n', 'j', function() LineMotion("j") end, { silent = true })
+vim.keymap.set('n', 'k', function() LineMotion("k") end, { silent = true })
+
+-- Reload Neovim configuration
+vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC<CR>")
+
+-- Copy the whole file
+vim.keymap.set("n", "<leader>y", "ggyG``")
+
+-- Visually select last-pasted text
+vim.keymap.set("n", "gV", "`[v`]")
+
+-- Split line at cursor (the reverse operation to join lines [J])
+-- Modified from Steve Losh's vimrc at https://bitbucket.org/sjl/dotfiles/src/cbbbc897e9b3/vim/vimrc
+vim.keymap.set("n", "S", "i<CR><ESC>gk:silent! s/\v +$//<CR>gj^`.")
+
+-- Easier completion
+vim.keymap.set("i", "<C-o>", "<C-x><C-o>", { silent = true})
+vim.keymap.set("i", "<C-f>", "<C-x><C-f>", { silent = true})
+vim.keymap.set("i", "<C-l>", "<C-x><C-l>", { silent = true})
+
+-- Insert current date
+vim.keymap.set("i", "<C-d>", "<C-r>=strftime('%F')<CR>", { silent = true })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -178,3 +178,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.colorcolumn = false
   end,
 })
+
+
+-- Set filetype of .tex files to LaTeX:
+vim.g.tex_flavor = "latex"
