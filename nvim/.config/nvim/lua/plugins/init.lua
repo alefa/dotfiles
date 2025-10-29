427a76ee89a4c920203b81aa4e36a877e5107dcb
Gita Benadi <gita.benadi@posteo.de>
2025-10-29 08:51:44 +0100

    Neovim: remove LSP config and completion, add gitsigns
    LSP needs to be configured differently in Neovim >= 0.11. The new
    configuration should be simpler and require fewer plugins, but I don't
    have time to set this up now.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 -- Plugins that don't have custom configurations are listed here
 return {
 	"freitass/todo.txt-vim",
-	"tpope/vim-unimpaired"
+	"tpope/vim-unimpaired",
+    "lewis6991/gitsigns.nvim"
 }
